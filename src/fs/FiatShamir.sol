36af4f92800b8575aa7ba5aaefe3c92cc5088dec
dmpierre <pdaixmoreux@gmail.com>
2024-10-22 16:49:24 +0200

    feat: arthur fs working

--- src/fs/FiatShamir.sol
+++ src/fs/FiatShamir.sol
@@ -2,23 +2,82 @@
 pragma solidity ^0.8.20;
 
 import {BN254} from "solidity-bn254/BN254.sol";
-import {console} from "forge-std/Test.sol";
 
-library FiatShamir {
-    /// @notice Derive `n` challenges using `scalars.length` provided values.
-    function deriveChallengesFromScalars(BN254.ScalarField[] memory scalars, uint32 n)
+struct Arthur {
+    bytes transcript;
+    bytes state;
+    uint256 cur; // pointer to current transcript bytes
+}
+
+/// @notice Provides fs utilities, Arthur only
+/// @dev API slightly differs from what has been implemented on the whir side
+library EVMFs {
+    function newArthur() external pure returns (Arthur memory) {
+        Arthur memory arthur;
+        return arthur;
+    }
+
+    function squeezeScalars(Arthur memory arthur, uint32 n)
         external
         pure
-        returns (BN254.ScalarField[] memory)
+        returns (Arthur memory, BN254.ScalarField[] memory)
     {
-        require(scalars.length > 0);
         BN254.ScalarField[] memory challenges = new BN254.ScalarField[](n);
-        bytes32 challengeBytes = keccak256(abi.encodePacked(scalars, uint32(0)));
+        bytes32 challengeBytes = keccak256(abi.encodePacked(arthur.state, uint32(0)));
         challenges[0] = BN254.ScalarField.wrap(uint256(challengeBytes));
         for (uint32 i = 1; i < n; i++) {
-            challengeBytes = keccak256(abi.encodePacked(challengeBytes, i));
+            challengeBytes = keccak256(abi.encodePacked(challengeBytes, uint32(i)));
             challenges[i] = BN254.ScalarField.wrap(uint256(challengeBytes));
         }
-        return challenges;
+        delete arthur.state;
+        return (arthur, challenges);
+    }
+
+    function squeezeBytes(Arthur memory arthur, uint32 n) external pure returns (Arthur memory, bytes32[] memory) {
+        bytes32[] memory challenges = new bytes32[](n);
+        bytes32 challengeBytes = keccak256(abi.encodePacked(arthur.state, uint32(0)));
+        challenges[0] = challengeBytes;
+        for (uint32 i = 1; i < n; i++) {
+            challengeBytes = keccak256(abi.encodePacked(challengeBytes, uint32(i)));
+            challenges[i] = challengeBytes;
+        }
+        delete arthur.state;
+        return (arthur, challenges);
+    }
+
+    function bytesToScalar(bytes memory scalar) private pure returns (BN254.ScalarField) {
+        return BN254.ScalarField.wrap(uint256(bytes32(scalar)));
+    }
+
+    function nextBytes(Arthur memory arthur, uint128 n) external pure returns (Arthur memory, bytes memory) {
+        arthur.state = new bytes(n);
+        bytes memory res = new bytes(n);
+        for (uint128 i = 0; i < n; i++) {
+            arthur.state[i] = arthur.transcript[arthur.cur + i];
+            res[31 - i] = arthur.transcript[arthur.cur + i];
+        }
+        arthur.cur += n; // transcript pointer goes forward
+        return (arthur, res);
+    }
+
+    // @notice BN254.ScalarField elements are serialized into 32 bytes array
+    function nextScalars(Arthur memory arthur, uint128 n)
+        external
+        pure
+        returns (Arthur memory, BN254.ScalarField[] memory)
+    {
+        BN254.ScalarField[] memory scalars = new BN254.ScalarField[](n);
+        arthur.state = new bytes(n * 32);
+        for (uint128 i = 0; i < n * 32; i += 32) {
+            bytes memory scalar = new bytes(32);
+            for (uint128 j = 0; j < 32; j++) {
+                uint256 idx = i + j;
+                arthur.state[idx] = arthur.transcript[arthur.cur + idx];
+                scalar[j] = arthur.transcript[arthur.cur + idx];
+            }
+            scalars[i / 32] = bytesToScalar(scalar);
+        }
+        arthur.cur += n * 32; // transcript pointer goes forward
+        return (arthur, scalars);
     }
 }
--- test/fs/FiatShamir.t.sol
+++ test/fs/FiatShamir.t.sol
@@ -3,67 +3,59 @@ pragma solidity ^0.8.20;
 
 import {BN254} from "solidity-bn254/BN254.sol";
 
+import {console} from "forge-std/Test.sol";
 import {WhirBaseTest} from "../WhirBaseTest.t.sol";
-import {FiatShamir} from "../../src/fs/FiatShamir.sol";
+import {EVMFs, Arthur} from "../../src/fs/FiatShamir.sol";
 
-contract FiatShamirTest is WhirBaseTest {
-    /// @notice test values checked against rust implementation
-    function test_deriveChallengesFromScalar_singleScalar() external pure {
-        // derive single challenge
-        BN254.ScalarField[] memory toHash = new BN254.ScalarField[](1);
-        toHash[0] = BN254.ScalarField.wrap(42);
-        BN254.ScalarField[] memory challenge = FiatShamir.deriveChallengesFromScalars(toHash, 1);
-        assertEqUintScalarField(
-            729468372965156355555354389332233798089583474701725319369131474314595715038, challenge[0]
-        );
+contract EVMFsTest is WhirBaseTest {
+    // @notice test values taken from rust implem
+    function test_arthur() external pure {
+        Arthur memory arthur = EVMFs.newArthur();
+        bytes memory transcript = new bytes(160);
+        for (uint8 i = 0; i < 160; i++) {
+            transcript[i] = bytes1(i);
+        }
+        arthur.transcript = transcript;
+        BN254.ScalarField[] memory scalars = new BN254.ScalarField[](2);
+        bytes memory b = new bytes(32);
+        bytes32[] memory bytesArray = new bytes32[](2);
 
-        // derive five challenges
-        BN254.ScalarField[] memory challenges = FiatShamir.deriveChallengesFromScalars(toHash, 5);
+        (arthur, scalars) = EVMFs.nextScalars(arthur, 2);
+        assertEqUintScalarField(1780731860627700044960722568376592200742329637303199754547598369979440671, scalars[0]);
         assertEqUintScalarField(
-            729468372965156355555354389332233798089583474701725319369131474314595715038, challenges[0]
+            14532552714582660066924456880521368950258152170031413196862950297402215317055, scalars[1]
+        );
+        assertEq(arthur.cur, 64);
+        (arthur, scalars) = EVMFs.squeezeScalars(arthur, 2);
+        assertEqUintScalarField(
+            12683060701355308782706700515672074170454102507116826504056080700491237103607, scalars[0]
         );
         assertEqUintScalarField(
-            18936102730406357255318628827011331276731862500286706093933432411019434973466, challenges[1]
+            13839230134794589166250157080361894918759939301567037118679620098290800826094, scalars[1]
+        );
+        (arthur, scalars) = EVMFs.nextScalars(arthur, 1);
+        assertEq(arthur.cur, 96);
+        assertEqUintScalarField(
+            7175081825465417211557547293217086219767197610009488850273148809858642697822, scalars[0]
+        );
+        (arthur, scalars) = EVMFs.squeezeScalars(arthur, 2);
+        assertEqUintScalarField(
+            1771673146623460978843697473455519743229658119711270841913389261238928066891, scalars[0]
         );
         assertEqUintScalarField(
-            6541929584115784620327860332127424392167657064562883012690568595760249538119, challenges[2]
-        );
-        assertEqUintScalarField(
-            20777190247772607223816048268084911929521105146019479864392516211965679784298, challenges[3]
-        );
-        assertEqUintScalarField(
-            9342720647770071610477633136826579252885066390043962637971223879036655112408, challenges[4]
-        );
-    }
-
-    /// @notice test values checked against rust implementation
-    function test_deriveChallengesFromScalar_multipleScalars() external pure {
-        // derive single challenge
-        BN254.ScalarField[] memory toHash = new BN254.ScalarField[](3);
-        toHash[0] = BN254.ScalarField.wrap(42);
-        toHash[1] = BN254.ScalarField.wrap(24);
-        toHash[2] = BN254.ScalarField.wrap(42);
-        BN254.ScalarField[] memory challenge = FiatShamir.deriveChallengesFromScalars(toHash, 1);
-        assertEqUintScalarField(
-            20272601219932863375093832544253509814432610329127347624764401756649612871576, challenge[0]
+            13414504394749035439026830226758505404562455097332190602756292500867339601090, scalars[1]
         );
 
-        // derive five challenges
-        BN254.ScalarField[] memory challenges = FiatShamir.deriveChallengesFromScalars(toHash, 5);
-        assertEqUintScalarField(
-            20272601219932863375093832544253509814432610329127347624764401756649612871576, challenges[0]
-        );
-        assertEqUintScalarField(
-            11349770981960638342480498240062427971964806006024921722248107767029995752990, challenges[1]
-        );
-        assertEqUintScalarField(
-            7405114114690130500248438548362118856642710846147556793475009453302143762840, challenges[2]
-        );
-        assertEqUintScalarField(
-            263282946699401980800144399449301130259968353935879232687544294237552369384, challenges[3]
-        );
-        assertEqUintScalarField(
-            11707855677730236955305714024852848768653892714154937753505439974607830409137, challenges[4]
-        );
+        (arthur, b) = EVMFs.nextBytes(arthur, 32);
+        assertEq(arthur.cur, 128);
+        assertEq(b, hex"7f7e7d7c7b7a797877767574737271706f6e6d6c6b6a69686766656463626160");
+        (arthur, scalars) = EVMFs.squeezeScalars(arthur, 1);
+        assertEqUintScalarField(40276879530910984420319267942312546383818757582133881715188209916716224991, scalars[0]);
+        (arthur, b) = EVMFs.nextBytes(arthur, 32);
+        assertEq(b, hex"9f9e9d9c9b9a999897969594939291908f8e8d8c8b8a89888786858483828180");
+        assertEq(arthur.cur, 160);
+        (arthur, bytesArray) = EVMFs.squeezeBytes(arthur, 2);
+        assertEq(bytesArray[0], hex"739dee39dfe0e4116cf2fa867fcc4d4f985eb00353a7f5d164c8933d0f53c840");
+        assertEq(bytesArray[1], hex"2c49be39f1c3b49c2aa0f911b1c9a494d68124585d2cc86ce890f2bc79628706");
     }
 }
